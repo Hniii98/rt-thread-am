c516bc84a80a1494e81adf1105002057b9a3414c
hniii98 <752608928@qq.com>
2025-11-24 12:22:19 +0900

    feat: support context switch via pcb

--- bsp/abstract-machine/src/context.c
+++ bsp/abstract-machine/src/context.c
@@ -4,21 +4,29 @@
 
 #define STACK_SIZE 1024 * 4
 
-static Context **src = NULL;
-static Context **dst = NULL;
+// static Context **src = NULL;
+// static Context **dst = NULL;
 
 typedef struct {
   void *entry;
   void *parameter;
   void *exit;
-} Pack;
+} ParamsPack;
+
+typedef struct {
+  rt_ubase_t from;
+  rt_ubase_t to;
+} SwitchPack;
+
+
+
 
 static void __wrapper(void *arg) {
-  // Unpack parameters.
-  Pack *unpack = (Pack *)arg;
-  void *param = (void *)unpack->parameter;
-  void (*tentry)(void *) = (void (*)(void *))unpack->entry;
-  void (*texit)() = (void (*)())unpack->exit;
+  // Unpack paramter pack.
+  ParamsPack *ppack = (ParamsPack *)arg;
+  void *param = (void *)ppack->parameter;
+  void (*tentry)(void *) = (void (*)(void *))ppack->entry;
+  void (*texit)() = (void (*)())ppack->exit;
 
   // Call function
   tentry(param);
@@ -28,8 +36,13 @@ static void __wrapper(void *arg) {
 static Context* ev_handler(Event e, Context *c) {
   switch (e.event) {
     case EVENT_YIELD:
-      if(src) *src = c;
-      c = *dst;
+      SwitchPack *spack =(SwitchPack *)rt_thread_self()->user_data;
+      if(spack->from){
+        // Store current context *.
+        *(Context **)spack->from = c;
+      }
+      // Switch contex
+      c = *(Context **)spack->to;
       break;
     case EVENT_IRQ_TIMER:
       // Do nothing for now.  
@@ -44,15 +57,39 @@ void __am_cte_init() {
 }
 
 void rt_hw_context_switch_to(rt_ubase_t to) {
-  src = NULL;
-  dst = (Context **)to;
+  // Fetch thread pcb and use it's private data to pass paramter.
+  rt_thread_t pcb = rt_thread_self();
+  rt_ubase_t *thread_temp = &pcb->user_data;
+
+  // Buffer it't original data.
+  rt_ubase_t buffer = *thread_temp;
+
+  // Store switch pack.
+  SwitchPack spack = {.from =(rt_ubase_t)NULL, .to = to};
+  *thread_temp =(rt_ubase_t)&spack;
+
   yield();
+
+  // Recover orginal data.
+  *thread_temp = buffer;
 }
 
 void rt_hw_context_switch(rt_ubase_t from, rt_ubase_t to) {
-  src = (Context **)from;
-  dst = (Context **)to;
+  // Fetch thread pcb and use it's private data to pass paramter.
+  rt_thread_t pcb = rt_thread_self();
+  rt_ubase_t *thread_temp = &pcb->user_data;
+
+  // Buffer it't original data.
+  rt_ubase_t buffer = *thread_temp;
+
+  // Store switch pack.
+  SwitchPack spack = {.from = from, .to = to};
+  *thread_temp =(rt_ubase_t)&spack;
+
   yield();
+
+  // Recover orginal data.
+  *thread_temp = buffer;
 }
 
 void rt_hw_context_switch_interrupt(void *context, rt_ubase_t from, rt_ubase_t to, struct rt_thread *to_thread) {
@@ -67,13 +104,13 @@ rt_uint8_t *rt_hw_stack_init(void *tentry, void *parameter, rt_uint8_t *stack_ad
   uintptr_t top = buttom - STACK_SIZE;
 
   // Pack parameters.
-  Pack *pack = (Pack *)top;
-  pack->entry = tentry;
-  pack->parameter = parameter;
-  pack->exit = texit;
+  ParamsPack *ppack = (ParamsPack *)top;
+  ppack->entry = tentry;
+  ppack->parameter = parameter;
+  ppack->exit = texit;
 
   // Construct a context at stack top
-  Context *c = kcontext((Area){(void *)top, (void *)buttom}, __wrapper, (void *)pack);
+  Context *c = kcontext((Area){(void *)top, (void *)buttom}, __wrapper, (void *)ppack);
 
   return (rt_uint8_t *)c;
 }
